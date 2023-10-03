0bd3bcb3b29aa6d638294e8e8188786189bd8e0e
JIHYUN <hyun8503@gmail.com>
2023-10-03 17:13:12 -0400

    succeed in fetching data from database

--- ninja-reviews/frontend/src/pages/Homepage.js
+++ ninja-reviews/frontend/src/pages/Homepage.js
@@ -1,5 +1,30 @@
 import React from "react";
+import useFetch from "../hooks/UseFetch";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
-  return <div>Homepage</div>;
+  const { loading, error, data } = useFetch(
+    "http://localhost:1337/api/reviews"
+  );
+
+  console.log(data);
+  console.log(data.data);
+  const content = data.data;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div>
+      {content.map((review) => (
+        <div key={review.attributes.name} className="review-card">
+          <div className="rating">{review.attributes.rating}</div>
+          <h2>{review.attributes.title}</h2>
+          <small>coonsole list</small>
+          <p>{review.attributes.body}</p>
+          <Link to={`/details/${review.attributes.name}`}>read more</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
